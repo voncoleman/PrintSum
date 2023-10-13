1b27a892e6a8a434971934a9863bc26ecd5adc5b
Von Coleman <von.coleman@yahoo.com>
2023-10-12 21:06:45 -0500

    first commit

---
+++ src/PrintSum.java
@@ -0,0 +1,5 @@
+public class PrintSum { //When you change the class name from App to whatever, be sure to change the name of the src file too!
+    public static void main(String[] args){  //My main man! The genesis, the beginning. 
+        System.out.println(24+90); //System.out.println displays the sum of the two number within the (). + operator does the addition!
+    }
+}
